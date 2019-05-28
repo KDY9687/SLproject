5f6a77778a76c18a0ba28be4ff690c89ee345de7
KDY_MyCom <jungsang3488@gmailc.om>
2019-05-29 00:11:29 +0900

    프로그램 제목 표시
    상단 (110, 0)

--- 약국찾기.py
+++ 약국찾기.py
@@ -5,4 +5,11 @@ import  tkinter.messagebox
 m_Tk = Tk()
 m_Tk.geometry("400x600")
 
+def IniteTopText():
+    TempFont =  font.Font(m_Tk, size = 15, weight = 'bold', family = 'Consolas')
+    MainText = Label(m_Tk, font = TempFont, text = "약국 검색 프로그램 ")
+    MainText.pack()
+    MainText.place(x = 110)
+
+IniteTopText()
 m_Tk.mainloop()
