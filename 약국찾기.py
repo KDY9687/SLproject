65c644a269a282bfd2cef3891f9b514ad56bee19
KDY_MyCom <jungsang3488@gmailc.om>
2019-05-29 08:22:00 +0900

    URL연동
    파싱 해결중

--- 약국찾기.py
+++ 약국찾기.py
@@ -1,18 +1,42 @@
 from tkinter import*
 from tkinter import font
-import tkinter.messagebox
+import http.client
 
 m_Tk = Tk()
 m_Tk.geometry("400x600")
+
+def SearchLibrary():
+    from xml.dom.minidom import parseString
+    conn = http.client.HTTPConnection("apis.data.go.kr")
+    conn.request("GET","/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire?"
+                       "Q0=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C"
+                       "&Q1=%EA%B0%95%EB%82%A8%EA%B5%AC"
+                       "&numOfRows=1"
+                       "&ORD=ADDR"
+                       "&pageNo=1"
+                       "&ServiceKey=hT3QoMtSEmueNDCV7mfsf0eYqEm1XfmluXaiuA8Uiw88V9k3kXsZpKb%2FqVjyLmGUA37%2Bl1LfxgTnB8bSt5frWg%3D%3D")
+    req = conn.getresponse()
+    print(req.status, req.reason)
+
+    response_body = req.read()
+    print(response_body.decode('utf-8'))
+
+    if req.status == 200:
+        MedcsDoc = req.read().decode('utf-8')
+        if MedcsDoc == None:
+            print("에러")
+        else:
+            #parseData = parseString(MedcsDoc)
+            #MedcLibrary = parseData.childNodes
+            #row = MedcLibrary[0].childNodes
+            print()
+
 
 def InitTopText():
     TempFont =  font.Font(m_Tk, size = 15, weight = 'bold', family = 'Consolas')
     MainText = Label(m_Tk, font = TempFont, text = "약국 검색 프로그램 ")
     MainText.pack()
     MainText.place(x = 110)
-
-def initSearchListBox():
-    pass
 
 def InitInputLabel():
     global InputLabel
@@ -41,11 +65,9 @@ def InitButton():
 
 
 def SearchButtonAction():
-    global SearchListBox
-
     RenderText.configure(state = 'normal')
     RenderText.delete(0.0, END)
-    initSearchIndex = SearchListBox.curselection()[0]
+    SearchLibrary()
 
     RenderText.configure(state = 'disabled')
 
@@ -69,6 +91,7 @@ def IniteRenderText():
 InitTopText()
 InitInputLabel()
 InitButton()
+SearchLibrary()
 #SearchButtonAction()
 IniteRenderText()
 m_Tk.mainloop()
